ad220aab64a7e24dfd8e6999c2a83989d5df756d
Aswin Reji <aswinreji.004@gmail.com>
2024-08-03 23:42:47 +0530

    Postgres learning basics operation

---
+++ Postgres.sql
@@ -0,0 +1,13 @@
+-- Ceate a Table 
+
+CREATE TABLE <TABLE NAME> (
+  name type
+)
+
+
+-- Insert Value in Table
+
+INSERT INTO <TABLE NAME> VALUES (name) -- If type is character we need to  use '' (quotes) if it is number no need for '' 
+
+
+
