1a9eff5c5b1c5bde3240f5f0102b96fe4b55388b
Aswin Reji <aswinreji.004@gmail.com>
2024-08-04 00:43:09 +0530

    Postgres learning basics operation

--- Postgres.sql
+++ Postgres.sql
@@ -10,4 +10,13 @@ CREATE TABLE <TABLE NAME> (
 INSERT INTO <TABLE NAME> VALUES (name) -- If type is character we need to  use '' (quotes) if it is number no need for '' 
 
 
+-- Sample Workout 
+
+CREATE TABLE cars (
+  name varchar(20)    -- 20 length of the String
+  model varchar(30)     -- It should be be above 2024
+  price int
+)
+
+INSERT INTO cars ValUES ('Mustang','Ford',280000)
 
